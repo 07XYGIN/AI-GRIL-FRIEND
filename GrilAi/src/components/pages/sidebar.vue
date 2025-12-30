3b5d67f27bf25387928b9d96913235a5d14f4247
Gin <xygin0708@163.com>
2025-12-30 14:23:29 +0800

    refactor: update sidebar and chat components for improved layout and functionality
    - Removed Theme component from sidebar menu item and added it to the header for better visibility.
    - Adjusted sidebar header to be sticky with a background for enhanced user experience.
    - Refactored chat component to display messages dynamically with a new input area for sending messages.
    - Implemented message sending functionality with user input handling.

--- GrilAi/src/components/pages/sidebar.vue
+++ GrilAi/src/components/pages/sidebar.vue
@@ -30,7 +30,6 @@ import Theme from './theme.vue';
                             </div>
                             <div class=" flex flex-1 text-left text-sm leading-tight justify-between items-center">
                                 <span class="truncate font-semibold">Gin friend</span>
-                                <Theme />
                             </div>
                         </SidebarMenuButton>
                     </SidebarMenuItem>
@@ -60,12 +59,13 @@ import Theme from './theme.vue';
             <SidebarRail />
         </Sidebar>
         <SidebarInset>
-            <header class="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear">
+            <header class="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear sticky top-0 bg-background z-10 ">
                 <div class="flex items-center gap-2 px-4">
                     <SidebarTrigger class="-ml-1" />
+                    <Theme />
                 </div>
             </header>
-            <main class="p-3 h-full">
+            <main class="">
                 <router-view></router-view>
             </main>
         </SidebarInset>
--- GrilAi/src/pages/chat.vue
+++ GrilAi/src/pages/chat.vue
@@ -1,42 +1,79 @@
 <template>
-    <div class="w-full h-full flex flex-col justify-between">
-        <div>
-            消息列表
-        </div>
-        <div class="flex justify-center">
-            <div class="grid w-full max-w-lg gap-6">
-                <InputGroup>
-                    <InputGroupTextarea placeholder="Ask, Search or Chat..." />
-                    <InputGroupAddon align="block-end">
-                        <InputGroupButton variant="outline" class="rounded-full" size="icon-xs">
-                            <PlusIcon class="size-4" />
-                        </InputGroupButton>
-                        <DropdownMenu>
-                            <DropdownMenuTrigger as-child>
-                                <InputGroupButton variant="ghost">
-                                    Auto
-                                </InputGroupButton>
-                            </DropdownMenuTrigger>
-                        </DropdownMenu>
-                        <InputGroupText class="ml-auto">
-                        </InputGroupText>
-                        <Separator orientation="vertical" />
-                        <InputGroupButton variant="default" class="rounded-full" size="icon-xs">
-                            <ArrowUpIcon class="size-4" />
-                            <span class="sr-only">Send</span>
-                        </InputGroupButton>
-                    </InputGroupAddon>
-                </InputGroup>
+    <div class="w-full h-full flex flex-col justify-between px-16 relative">
+        <div class="">
+            <div v-for="(item, index) in response.data" :key="index" class="p-2"
+                :class="item.role === 'user' ? 'text-right' : 'text-left'">
+                <div
+                    :class="item.role === 'user' ? 'inline-block  text-foreground rounded-lg p-2' : 'inline-block  text-foreground rounded-lg p-2'">
+                    {{ item.content }}
+                </div>
+            </div>
+            <div class="bg-black w-7xl fixed bottom-0">
+                <div class="grid gap-6">
+                    <InputGroup>
+                        <InputGroupTextarea placeholder="输入内容开始聊天......" v-model="msg" />
+                        <InputGroupAddon align="block-end">
+                            <InputGroupButton variant="outline" class="rounded-full" size="icon-xs">
+                                <PlusIcon class="size-4" />
+                            </InputGroupButton>
+                            <DropdownMenu>
+                                <DropdownMenuTrigger as-child>
+                                    <InputGroupButton variant="ghost">
+                                        Auto
+                                    </InputGroupButton>
+                                </DropdownMenuTrigger>
+                            </DropdownMenu>
+                            <InputGroupText class="ml-auto">
+                            </InputGroupText>
+                            <Separator orientation="vertical" />
+                            <InputGroupButton variant="default" class="rounded-full" size="icon-xs" @click="send"
+                                @click.enter="send">
+                                <ArrowUpIcon class="size-4" />
+                                <span class="sr-only">Send</span>
+                            </InputGroupButton>
+                        </InputGroupAddon>
+                    </InputGroup>
+                </div>
             </div>
         </div>
-    </div>
+        </div>
 </template>
 
 <script setup lang="ts">
-import { ArrowUpIcon,PlusIcon,} from 'lucide-vue-next'
+import { ArrowUpIcon, PlusIcon, } from 'lucide-vue-next'
 import { DropdownMenu, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
 import { InputGroup, InputGroupAddon, InputGroupButton, InputGroupText, InputGroupTextarea } from '@/components/ui/input-group'
 import { Separator } from '@/components/ui/separator'
+import { ref } from 'vue'
+const msg = ref<string>("")
+const response = ref({
+    code: 200,
+    msg: "OK",
+    data: [
+        {
+            role: "user",
+            content: "Hello, how are you?"
+        },
+        {
+            role: "assistant",
+            content: "I'm fine, thank you! How can I assist you today?"
+        },
+        {
+            role: "user",
+            content: "I'm fine, thank you! How can I assist you today?"
+        },
+        
+    ]
+})
+
+const send = () => {
+    if (!msg.value) return
+    response.value.data.push({
+        role: "user",
+        content: msg.value
+    })
+    msg.value = ""
+}
 </script>
 
 <style lang="scss" scoped></style>
